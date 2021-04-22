da7fe913bac85c135d2342b0a3125bbfa743d395
DilipKheni10001 <discussionforall@gmail.com>
2021-04-22 15:13:18 +0530

    set box

--- src/Component/notebooks/NoteContainer.js
+++ src/Component/notebooks/NoteContainer.js
@@ -24,7 +24,15 @@ const NoteContainer = () => {
                     notebooks.map(book=>{
                            return (
                                 <>
-                                    <div className="notebook-heading">{"#"}<input text="text" className="node" value={book.value} onChange={(e)=>updateNotebookName(e,book)}/><span onClick={() => deleteNotebook(book)} className="crossIcon">&#10005;</span></div>
+                                
+                                    <div className="notebook-heading">
+                                        <button className="notebook-btn">+ note</button>
+                                        <button className="notebook-btn">+ image</button>
+                                        <span onClick={() => deleteNotebook(book)} className="crossIcon">&#10005;</span>
+                                    </div>
+                                    <div className="notebook-heading">
+                                        {"#"}<input text="text" className="input-box" value={book.value} onChange={(e)=>updateNotebookName(e,book)}/>
+                                    </div>
                                     <textarea className="text-content" rows="10" onChange={(e)=>updateNotebookContent(e,book)} value={book.content}></textarea>        
                                 
                                 </>
